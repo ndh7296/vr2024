3ed52439fa56e17e625aa60ba16352e20cffc471
ndh7296 <dhlrkrwleo@gmail.com>
2024-10-17 13:27:00 +0900

    업로드1

---
+++ data.js
@@ -0,0 +1,751 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2",
+      "name": "파노라마2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.07074828331731098,
+        "pitch": 0,
+        "fov": 1.5396019870895838
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.21338468713696912,
+          "pitch": -0.006060432498994572,
+          "rotation": 0,
+          "target": "1-14"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.803146619270958,
+          "pitch": 0.0026077395807160286,
+          "title": "소공원",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.847805496271592,
+          "pitch": -0.017540190315605386,
+          "title": "주차장",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-14",
+      "name": "파노라마14",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5765849914440064,
+          "pitch": 0.10438843991569513,
+          "rotation": 0,
+          "target": "2-15"
+        },
+        {
+          "yaw": -3.001199675859244,
+          "pitch": 0.12019384819740964,
+          "rotation": 0,
+          "target": "0-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8811030258923562,
+          "pitch": -0.03967693824747265,
+          "title": "사무실",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.8102382346456025,
+          "pitch": -0.019139116898388764,
+          "title": "면회실",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-15",
+      "name": "파노라마15",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5274550934374993,
+          "pitch": 0.007819780159803003,
+          "rotation": 0,
+          "target": "3-9"
+        },
+        {
+          "yaw": -2.5865245447741785,
+          "pitch": -0.006937581034623719,
+          "rotation": 0,
+          "target": "1-14"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4992326455922722,
+          "pitch": 0.07345858147294315,
+          "title": "주방",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.4194296267251332,
+          "pitch": 0.000060674159357176904,
+          "title": "식당",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.43549977175667287,
+          "pitch": 0.21833995450401034,
+          "title": "로비",
+          "text": "면회대기, 휴식"
+        },
+        {
+          "yaw": -0.34649263704118205,
+          "pitch": 0.01117715683116316,
+          "title": "면회실",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.238666410315794,
+          "pitch": 0.00971497523421938,
+          "title": "면회실",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.2671707349847896,
+          "pitch": 0.012367112829974047,
+          "title": "사무실",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-9",
+      "name": "파노라마9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3576800890520744,
+          "pitch": 0.08677895216505505,
+          "rotation": 0,
+          "target": "2-15"
+        },
+        {
+          "yaw": -0.1954250528837278,
+          "pitch": 0.06581839045334092,
+          "rotation": 0,
+          "target": "4-10"
+        },
+        {
+          "yaw": -2.017978082969911,
+          "pitch": 0.230757466359643,
+          "rotation": 0,
+          "target": "6-16"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.284101580919785,
+          "pitch": 0.06329708451786331,
+          "title": "E.V",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.6475736638824472,
+          "pitch": 0.1097324623140139,
+          "title": "공용화장실",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.636765404184997,
+          "pitch": 0.029837197755496092,
+          "title": "계단실",
+          "text": "Text"
+        },
+        {
+          "yaw": -3.0140252824459566,
+          "pitch": 0.027265874839294213,
+          "title": "상담실",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-10",
+      "name": "파노라마10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9287811324100197,
+          "pitch": 0.10377556880660066,
+          "rotation": 0,
+          "target": "5-3_000"
+        },
+        {
+          "yaw": -2.676032414114921,
+          "pitch": -0.02078204100588721,
+          "rotation": 0,
+          "target": "3-9"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5851782998271489,
+          "pitch": 0.04839235895263272,
+          "title": "연못",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-3_000",
+      "name": "파노라마3_000",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5345184497537527,
+          "pitch": 0.08679356144087791,
+          "rotation": 0,
+          "target": "4-10"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.041755602310828976,
+          "pitch": -0.032118612699882476,
+          "title": "주차장 쪽문",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-16",
+      "name": "파노라마16",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5754119749657054,
+          "pitch": 0.4651883478419343,
+          "rotation": 0,
+          "target": "3-9"
+        },
+        {
+          "yaw": 0.07289728357704739,
+          "pitch": 0.11260156611657379,
+          "rotation": 0,
+          "target": "8-19"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7094590765046256,
+          "pitch": 0.10100175530565636,
+          "title": "보호사",
+          "text": "안전관리"
+        },
+        {
+          "yaw": -0.787103806417818,
+          "pitch": -0.05839609654780631,
+          "title": "객실",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.2216814249722745,
+          "pitch": -0.06497070949437322,
+          "title": "객실",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.9843248792729895,
+          "pitch": 0.4043044471261741,
+          "title": "핸드레일",
+          "text": "노인분들 이동 보조"
+        }
+      ]
+    },
+    {
+      "id": "7-18_000",
+      "name": "파노라마18_000",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1509442640365357,
+          "pitch": 0.08257389619657296,
+          "rotation": 0,
+          "target": "8-19"
+        },
+        {
+          "yaw": 3.1397614342159645,
+          "pitch": 0.05640619051584039,
+          "rotation": 0,
+          "target": "6-16"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2300160620242515,
+          "pitch": 0.16166909843402522,
+          "title": "난간",
+          "text": "답답함을 해소하기 위해 바닥을&nbsp;<div>뚫었음</div>"
+        },
+        {
+          "yaw": 0.4234119793624984,
+          "pitch": 0.31830908656358403,
+          "title": "공용거실",
+          "text": ""
+        },
+        {
+          "yaw": 1.3896744186704506,
+          "pitch": 0.24396160341769146,
+          "title": "핸드레일",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "8-19",
+      "name": "파노라마19",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8200638633875394,
+          "pitch": 0.2837546662260735,
+          "rotation": 0,
+          "target": "9-20_000"
+        },
+        {
+          "yaw": -0.22363490135418118,
+          "pitch": 0.19659634392319703,
+          "rotation": 0,
+          "target": "7-18_000"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-20_000",
+      "name": "파노라마20_000",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7933260285932136,
+          "pitch": 0.22196959592745102,
+          "rotation": 0,
+          "target": "8-19"
+        },
+        {
+          "yaw": 0.7338953605958096,
+          "pitch": 0.0864415953558364,
+          "rotation": 0,
+          "target": "10-22"
+        },
+        {
+          "yaw": 2.800090204781183,
+          "pitch": 0.02236498510063356,
+          "rotation": 0,
+          "target": "12-24"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.002943346381673706,
+          "pitch": 0.1067293468602859,
+          "title": "프로그램실",
+          "text": "노인분들을 위한 프로그램을 진행"
+        }
+      ]
+    },
+    {
+      "id": "10-22",
+      "name": "파노라마22",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8121006947678335,
+          "pitch": 0.2464713071885214,
+          "rotation": 0,
+          "target": "9-20_000"
+        },
+        {
+          "yaw": -0.19367546291640636,
+          "pitch": 0.11591417247089097,
+          "rotation": 0,
+          "target": "11-23"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.1931770209527155,
+          "pitch": -0.06863681722139958,
+          "title": "썬룸",
+          "text": "야외활동을 꺼려하는 분들을 위한 썬룸"
+        }
+      ]
+    },
+    {
+      "id": "11-23",
+      "name": "파노라마23",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6270363146218134,
+          "pitch": 0.17333593024032012,
+          "rotation": 0,
+          "target": "10-22"
+        },
+        {
+          "yaw": -0.5374364254688384,
+          "pitch": 0.015172603822781738,
+          "rotation": 0,
+          "target": "9-20_000"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.08291428818155389,
+          "pitch": 0.18439812745746842,
+          "title": "핸드레일",
+          "text": "간단한 걷기운동"
+        }
+      ]
+    },
+    {
+      "id": "12-24",
+      "name": "파노라마24",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3351611915119648,
+          "pitch": 0.4751127654786558,
+          "rotation": 0,
+          "target": "9-20_000"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3788011713500321,
+          "pitch": 0.20100501323944187,
+          "title": "옥상정원",
+          "text": "야외활동이 힘든 날에<div>야외 프로그램을 할 수있는 공간</div>"
+        }
+      ]
+    }
+  ],
+  "name": "요양원 VR",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
